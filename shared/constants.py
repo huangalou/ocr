2fded599de3d395cb3391ca13f28971d41beeec4
susualou <alou.huang@gmail.com>
2026-03-30 13:48:07 +0800

    feat: add YouTube video job constants and env vars

--- shared/constants.py
+++ shared/constants.py
@@ -1,3 +1,7 @@
 REDIS_QUEUE_FRAMES = "queue:frames"
 REDIS_CHANNEL_PLATES = "channel:plate_recognized"
 MINIO_BUCKET = "ocr-images"
+
+REDIS_QUEUE_VIDEO_JOBS = "queue:video_jobs"
+REDIS_CHANNEL_VIDEO_PROGRESS = "channel:video_progress"
+VIDEO_STREAM_THRESHOLD_SEC = 300
