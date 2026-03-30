a720cd24cf3be0e74092a429e7cbb2cac171605d
susualou <alou.huang@gmail.com>
2026-03-30 15:03:44 +0800

    feat: add video_frames and plate_crops queue constants

--- shared/constants.py
+++ shared/constants.py
@@ -5,3 +5,6 @@ MINIO_BUCKET = "ocr-images"
 REDIS_QUEUE_VIDEO_JOBS = "queue:video_jobs"
 REDIS_CHANNEL_VIDEO_PROGRESS = "channel:video_progress"
 VIDEO_STREAM_THRESHOLD_SEC = 300
+
+REDIS_QUEUE_VIDEO_FRAMES = "queue:video_frames"
+REDIS_QUEUE_PLATE_CROPS = "queue:plate_crops"
